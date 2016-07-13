ad38b47627e0007eb6ce5257d6a770cd4008b604
t-kubrak <chikashikaari@gmail.com>
2016-07-12 22:30:01 -0400

    Update animations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(".solarSection:first-child").hover(function(){
+$(".solarSection").slice(0,1).hover(function(){
 	$('main .line').slice(0,2)
 		.css("border-color", "white")
 		.css("transition", "0.4s");
@@ -14,7 +14,7 @@ $(".solarSection:first-child").hover(function(){
 		.css("transition", "0.4s");
 });
 
-$(".lunarSection:first-child").hover(function(){
+$(".lunarSection").slice(0,1).hover(function(){
 	$('.sunsetLine').slice(0,2)
 		.css("border-color", "white")
 		.css("transition", "0.4s");
@@ -30,7 +30,7 @@ $(".lunarSection:first-child").hover(function(){
     	.css("transition", "0.4s");
 });
 
-$(".solarSection:nth-child(2)").hover(function(){
+$(".solarSection").slice(1,2).hover(function(){
 	$('main .line').slice(1,3)
 		.css("border-color", "white")
 		.css("transition", "0.4s");
@@ -46,7 +46,7 @@ $(".solarSection:nth-child(2)").hover(function(){
 		.css("transition", "0.4s");
 });
 
-$(".lunarSection:nth-child(2)").hover(function(){
+$(".lunarSection").slice(1,2).hover(function(){
 	$('.sunsetLine').slice(1,3)
 		.css("border-color", "white")
 		.css("transition", "0.4s");
@@ -58,6 +58,72 @@ $(".lunarSection:nth-child(2)").hover(function(){
     	.css("border-color", "black")
     	.css("transition", "0.4s");
     $('.sunset').slice(1,3)
+    	.css("font-size", "3em")
+    	.css("transition", "0.4s");
+});
+
+/** 2nd ROW **/
+
+$(".solarSection").slice(2,3).hover(function(){
+	$('main .line').slice(3,5)
+		.css("border-color", "white")
+		.css("transition", "0.4s");
+	$('.midnight').slice(3,5)
+		.css("font-size", "18px")
+		.css("transition", "0.4s");
+}, function() {
+    $('main .line').slice(3,5)
+    	.css("border-color", "black")
+    	.css("transition", "0.4s");
+	$('.midnight').slice(3,5)
+		.css("font-size", "16px")
+		.css("transition", "0.4s");
+});
+
+$(".lunarSection").slice(2,3).hover(function(){
+	$('.sunsetLine').slice(3,5)
+		.css("border-color", "white")
+		.css("transition", "0.4s");
+	$('.sunset').slice(3,5)
+		.css("font-size", "3.5em")
+		.css("transition", "0.4s");
+}, function() {
+    $('.sunsetLine')
+    	.css("border-color", "black")
+    	.css("transition", "0.4s");
+    $('.sunset').slice(3,5)
+    	.css("font-size", "3em")
+    	.css("transition", "0.4s");
+});
+
+$(".solarSection").slice(3,4).hover(function(){
+	$('main .line').slice(4,6)
+		.css("border-color", "white")
+		.css("transition", "0.4s");
+	$('.midnight').slice(4,6)
+		.css("font-size", "18px")
+		.css("transition", "0.4s");
+}, function() {
+    $('main .line').slice(4,6)
+    	.css("border-color", "black")
+    	.css("transition", "0.4s");
+	$('.midnight').slice(4,6)
+		.css("font-size", "16px")
+		.css("transition", "0.4s");
+});
+
+$(".lunarSection").slice(3,4).hover(function(){
+	$('.sunsetLine').slice(4,6)
+		.css("border-color", "white")
+		.css("transition", "0.4s");
+	$('.sunset').slice(4,6)
+		.css("font-size", "3.5em")
+		.css("transition", "0.4s");
+}, function() {
+    $('.sunsetLine')
+    	.css("border-color", "black")
+    	.css("transition", "0.4s");
+    $('.sunset').slice(4,6)
     	.css("font-size", "3em")
     	.css("transition", "0.4s");
 });
